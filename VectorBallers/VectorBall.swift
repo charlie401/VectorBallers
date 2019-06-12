4b14bff5046220f4dbf9b84f1460108ee36be7f3
charlie401 <charlie404@163.com>
2019-06-12 08:55:22 +0800

    撒上
    as

--- VectorBallers/VectorBall.swift
+++ VectorBallers/VectorBall.swift
@@ -17,13 +17,16 @@ class VectorBall: CALayer {
     private var vector:(A:CGFloat,B:CGFloat) = (A:0,B:0)
     private var posit:(X:CGFloat,Y:CGFloat)   = (X:0,Y:0)
 
-    init(radius:CGFloat = 10, velocity:CGFloat = 1 ,mass:CGFloat = 1,vector:(X:CGFloat,Y:CGFloat) = (1,1), posit:(X:CGFloat,Y:CGFloat) = (1,1)) {
+    init(radius:CGFloat = 10, velocity:CGFloat = 1 ,mass:CGFloat = 1,vector:(A:CGFloat,B:CGFloat) = (1,1), posit:(X:CGFloat,Y:CGFloat) = (1,1)) {
         super.init()
         self.frame = CGRect.init(x: posit.X, y: posit.Y, width: radius, height: radius)
         self.radius = radius
         self.backgroundColor = UIColor.randomColor.cgColor
         self.cornerRadius = radius / 2.0
         self.velocity = velocity
+        self.vector = vector
+        self.posit = posit
+        self.mass = mass
         displayLink = CADisplayLink.init(target: self, selector: #selector(refreshFrame))
         displayLink.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
     }
@@ -54,6 +57,7 @@ class VectorBall: CALayer {
             return (0,0)
         }
         let dDis = velocity * CGFloat(intalval)
+        atan(vector.B / vector.A)
         let dx = (vector.A * dDis) / CGFloat(
                                             sqrt(
                                             Double(
