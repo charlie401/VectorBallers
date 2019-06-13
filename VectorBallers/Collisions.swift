ed8db7768db7d1d150b6da81cb651c5f9f593896
charlie401 <charlie404@163.com>
2019-06-13 15:06:05 +0800

    碰撞检测

---
+++ VectorBallers/Collisions.swift
@@ -0,0 +1,38 @@
+//
+//  Collisions.swift
+//  VectorBallers
+//
+//  Created by chongyang on 2019/6/13.
+//  Copyright © 2019年 chongyang. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class Collision {
+    
+    private var objectSet:Set<VectorBall> = Set<VectorBall>.init()
+    
+    private var objectRel:Dictionary<String,(A:VectorBall,B:VectorBall)> = Dictionary.init()
+    
+    private var distantInObjects:Dictionary<String,CGFloat> = Dictionary<String,CGFloat>.init()
+    
+    func collisionDetect(){
+        
+    }
+    
+    private func getRelId(a:VectorBall,b:VectorBall) -> String{
+        let ah = String.init(a.hashValue)
+        let bh = String.init(b.hashValue)
+        return ah > bh ? (ah + bh) : (bh + ah)
+    }
+    
+//    private func getObjectRel(id:String) -> (A:VectorBall,B:VectorBall)? {
+//        let res = objectRel[id]
+//        return res
+//    }
+    
+    func putObject(object:VectorBall){
+        
+    }
+    
+}
